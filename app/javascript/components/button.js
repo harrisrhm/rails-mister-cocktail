f33ce5e7ebcf32a25753a880b44cd72ae3f59e16
Harris Rahim <harris.rhm@gmail.com>
2020-09-06 09:30:48 +1000

    added plus/minus sign to the Cocktail List button

--- app/javascript/components/button.js
+++ app/javascript/components/button.js
@@ -1,17 +1,17 @@
 
 const initCollapseButtonOnClick = () => {
-    const coll = document.getElementsByClassName("list");
-    let i;
+const coll = document.getElementsByClassName("list");
+let i;
     for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
         const content = this.nextElementSibling;
-        if (content.style.display === "block") {
-        content.style.display = "none";
-        } else {
-        content.style.display = "block";
-        }
-    });
+            if (content.style.display === "block") {
+            content.style.display = "none";
+            } else {
+            content.style.display = "block";
+            }
+        });
     }
 }
 
